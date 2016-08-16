318bb1375ae9c8599b789ba21a4ff79ec172e693
lunlun1992 <lunlun1992@163.com>
2016-08-16 16:28:43 +0800

    Initial Commit

---
+++ FFmplayer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FFmplayer
+//
+//  Created by 冯伟伦 on 16/8/16.
+//  Copyright © 2016年 冯伟伦. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
